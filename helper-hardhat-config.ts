d61e4c027fc81727e1f385fddab246fc97d4b1aa
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-03-10 09:13:04 +0500

    Update helper-hardhat-config.ts file - Sepolia Update
    Replace rinkeby network configurations with sepolia

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -18,19 +18,19 @@ export const networkConfig: networkConfigInfo = {
     31337: {
         name: "localhost",
         subscriptionId: "588",
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
     },
-    4: {
-        name: "rinkeby",
+    11155111: {
+        name: "sepolia",
         subscriptionId: "588",
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
         keepersUpdateInterval: "30",
         raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
-        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab"
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625"
     },
     1: {
         name: "mainnet",
