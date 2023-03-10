d0f81a793ce869aebd99933ac066f25c202b3d16
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-03-10 08:16:05 +0500

    Update hardhat.config.js file - Sepolia Update
    Replace goerli network with sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,8 +14,8 @@ const MAINNET_RPC_URL =
     process.env.MAINNET_RPC_URL ||
     process.env.ALCHEMY_MAINNET_RPC_URL ||
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const GOERLI_RPC_URL =
-    process.env.GOERLI_RPC_URL || "https://eth-goerli.alchemyapi.io/v2/your-api-key"
+const SEPOLIA_RPC_URL =
+    process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
 const POLYGON_MAINNET_RPC_URL =
     process.env.POLYGON_MAINNET_RPC_URL || "https://polygon-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
@@ -40,14 +40,14 @@ module.exports = {
         localhost: {
             chainId: 31337,
         },
-        goerli: {
-            url: GOERLI_RPC_URL,
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
             //   accounts: {
             //     mnemonic: MNEMONIC,
             //   },
             saveDeployments: true,
-            chainId: 5,
+            chainId: 11155111,
         },
         mainnet: {
             url: MAINNET_RPC_URL,
